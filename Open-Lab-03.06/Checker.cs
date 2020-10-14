7e81b8538df14f6cfb17640193b52e66449d3d51
martinorion <71751917+martinorion@users.noreply.github.com>
2020-10-14 17:25:23 +0200

    if string contains a space return true, else return false

--- Open-Lab-03.06/Checker.cs
+++ Open-Lab-03.06/Checker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._06
 {
@@ -6,7 +6,12 @@ namespace Open_Lab_03._06
     {
         public bool HasSpaces(string str)
         {
-            throw new NotImplementedException();
+
+            if (str.Contains(" "))
+                return true;
+            else
+                return false;
+            
         }
     }
 }
